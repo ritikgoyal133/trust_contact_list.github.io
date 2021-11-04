08c6603813631fca4631069f67993e874a8ac3a5
ritikgoyal133 <ritikgoyal3092000@gmail.com>
2021-11-04 08:56:04 +0530

    deployed

---
+++ src/contacts.js
@@ -0,0 +1,62 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Ritik Goyal (Founder)",
+    imgURL:
+      "./ritik.jpg",
+    phone: "9837937000",
+    email: "ritikgoyal3092000@gmail.com"
+  },
+  {
+    id: 2,
+    name: "PavanVerma (Chairman)",
+    imgURL:
+    "./pavan.jpg",
+    phone: "7453817854",
+    email: "Pawanverma1132@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Himanshu (Manager)",
+    imgURL:
+      "./himanshu1.jpg",
+    phone: "8923646248",
+    email: "Himanshusharma12899@gmail.com"
+  },
+  {
+    id: 4,
+    name: "DevVarshney (Cashier)",
+    imgURL:
+      "./dev.jpg",
+    phone: "8534842336",
+    email: "Devgupta@gmail.com"
+  },
+  {
+    id: 5,
+    name: "ManishKumar (Member)",
+    imgURL:
+      "manish.jpg",
+    phone: "9058422305",
+    email: "manish786kumar@gmail.com"
+  },
+  
+  {
+    id: 6,
+    name: "AkashVarshney (Member)",
+    imgURL:
+      "./akash1.jpg",
+    phone: "9084791198",
+    email: "akash303001@gmail.com"
+  },
+  {
+    id: 7,
+    name: "RahulVarshney (Member)",
+    imgURL:
+      "./rahul.jpg",
+    phone: "6398922510",
+    email: "rahulvarshney671@gmail.com"
+  },
+  
+];
+
+export default contacts;
